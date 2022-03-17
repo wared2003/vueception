d578bed6a4dee1369628046ed50066e8dce6354a
Drouet <quentin.drouet109@gmail.com>
2022-03-17 15:15:49 +0100

    🚧 add animation when clicker clicked

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,9 +2,8 @@
     <div>
         <h2>Counter</h2>
         <div class="flex col-reverse">
-            <img @click="count++" class="imgCitrus" src="../assets/logo-active-citrus.svg" alt="">
-            <p>{{count}}</p>
-            
+          <img @click="count++" class="imgCitrus" src="../assets/logo-active-citrus.svg" alt="clicker logo">
+          <p>{{count}}</p>
         </div>
     </div>
 </template>
@@ -16,21 +15,30 @@
             return {
                 count: 0,
             }
-        }
+        },
     }
+
 </script>
 
 <style>
-    .flex{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        gap: 20px;
+    .flex {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: 20px;
     }
-    .col-reverse{
-        flex-direction: column-reverse;
+    .col-reverse {
+      flex-direction: column-reverse;
     }
-    .imgCitrus{
-        width: 10%;
+    .imgCitrus {
+      width: 150px;
+      cursor: pointer;
+      transition: all 0.3s;
+    }
+    .imgCitrus:hover {
+      transform: scale(1.20);
+    }
+    .imgCitrus:active {
+      transform: scale(1.02);
     }
 </style>
